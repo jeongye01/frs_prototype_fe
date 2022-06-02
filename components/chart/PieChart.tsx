bda333c8eadbe0fd45946c4ee205032b9099f853
정예원 <yewonn.1j@gmail.com>
2022-06-02 17:43:02 +0900

    pie init

--- components/chart/PieChart.tsx
+++ components/chart/PieChart.tsx
@@ -6,78 +6,85 @@ const ReactApexChart = dynamic(() => import('react-apexcharts'), {
 });
 function PieChart() {
   const state = {
-    series: [
-      {
-        data: [33, 33, 0],
-      },
-    ],
+    series: [33, 33, 0],
     options: {
-      grid: {
-        show: false,
-      },
-      plotOptions: {
-        bar: {
-          distributed: true,
-          horizontal: false,
-          columnWidth: '65%',
-          endingShape: 'rounded',
-          dataLabels: {
-            position: 'top',
-          },
-        },
-      },
-      column: {
-        colors: ['#662e8f', '#2e368f', '#5c7fd6'],
-      },
-      dataLabels: {
-        enabled: true,
-        position: 'top',
-        offsetY: -30,
-
-        style: {
-          fontSize: '16px',
-          colors: ['#662e8f', '#2e368f', '#5c7fd6'],
-        },
-      },
-
-      xaxis: {
-        categories: ['인증 요청', '인증 성공', '인증 실패'],
-        axisBorder: {
-          show: false,
-        },
-        tooltip: {
-          enabled: false,
-        },
-      },
-      yaxis: {
-        show: false,
-      },
-      fill: {
-        colors: ['#662e8f', '#2e368f', '#5c7fd6'],
-        opacity: 1,
-      },
       legend: {
         show: false,
       },
       chart: {
-        toolbar: {
-          show: false,
-          tools: {
-            download: false,
+        events: {
+          animationEnd: null,
+          beforeMount: null,
+          mounted: null,
+          updated: null,
+          mouseMove: null,
+          mouseLeave: null,
+          click: null,
+          legendClick: null,
+          markerClick: null,
+          selection: null,
+          dataPointSelection: null,
+          dataPointMouseEnter: null,
+          dataPointMouseLeave: null,
+          beforeZoom: null,
+          beforeResetZoom: null,
+          zoomed: null,
+          scrolled: null,
+        },
+      },
+      plotOptions: {
+        pie: {
+          expandOnClick: false,
+          donut: {
+            labels: {
+              show: true,
+              name: {
+                show: false,
+              },
+              value: {
+                show: true,
+                fontSize: '16px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                color: undefined,
+                offsetY: 0,
+              },
+              total: {
+                show: true,
+                label: 'Total',
+                color: '#373d3f',
+              },
+            },
           },
         },
       },
+      labels: ['인증 요청', '인증 성공', '인증 실패'],
+      dataLabels: {
+        enabled: false,
+      },
+
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 400,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        },
+      ],
     },
   };
 
   return (
     <div>
       <ReactApexChart
-        type="bar"
+        type="donut"
         series={state.series}
         options={state.options}
-        width="30%"
-        height="200px"
+        width="400px"
       />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import BarChart from 'components/chart/BarChart';
 import PieChart from 'components/chart/PieChart';
-
 const Home: NextPage = () => {
-  return <PieChart />;
+  return (
+    <div>
+      <BarChart />
+      <PieChart />
+    </div>
+  );
 };
 
 export default Home;

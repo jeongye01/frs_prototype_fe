42ad0bce531945dd357b0fe4bbdad55aa37abd45
정예원 <yewonn.1j@gmail.com>
2022-06-02 17:51:59 +0900

    create Table file

---
+++ components/Table.tsx
@@ -0,0 +1,4 @@
+function Table() {
+  return <></>;
+}
+export default Table;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import BarChart from 'components/chart/BarChart';
 import PieChart from 'components/chart/PieChart';
+import Table from 'components/Table';
 const Home: NextPage = () => {
-  return (
-    <div>
-      <BarChart />
-      <PieChart />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;

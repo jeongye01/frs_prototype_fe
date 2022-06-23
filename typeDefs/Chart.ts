822c6123c309f12e19d4e1e8193b2dad06672e01
정예원 <yewonn.1j@gmail.com>
2022-06-23 11:16:32 +0900

    Create Chart.ts

---
+++ typeDefs/Chart.ts
@@ -0,0 +1,14 @@
+export interface TodayFRType {
+  cfTotCnt: number;
+  cfPassCnt: number;
+  cfFailCnt: number;
+  cfPassRate: number;
+  cfFailRate: number;
+}
+export interface HistoryDailyFRType {
+  statDe: string;
+  reqeustCnt: number;
+  succesCnt: number;
+  failrCnt: number;
+  crttCls: null;
+}

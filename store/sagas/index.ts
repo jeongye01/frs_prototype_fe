4f2a64aa977bd35dff41e455192fc5886fd0d757
정예원 <yewonn.1j@gmail.com>
2022-06-23 14:25:46 +0900

    사가제거

--- store/sagas/index.ts
+++ store/sagas/index.ts
@@ -1,8 +1,7 @@
 import { all, fork } from 'redux-saga/effects';
-import historyFRSaga from './historyFRSaga';
 import userListSaga from './userListSaga';
 import userSaga from './userSaga';
 
 export default function* rootSaga() {
-  yield all([fork(historyFRSaga), fork(userListSaga), fork(userSaga)]);
+  yield all([fork(userListSaga), fork(userSaga)]);
 }

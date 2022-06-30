e1b86a06e8d306070cd33f1b98ffd9bc9021f8a3
정예원 <yewonn.1j@gmail.com>
2022-06-30 10:19:54 +0900

    검색 조건 초기화

--- components/Search.tsx
+++ components/Search.tsx
@@ -8,10 +8,10 @@ import React, {
 import historyFRSlice from 'store/slices/historyFRSlice';
 import { useAppDispatch } from 'hooks/redux';
 import { AxiosError } from 'axios';
-import { HistoryFRType } from 'typeDefs/HistoryFR';
 import { useQuery } from 'react-query';
 import { getHistoryFR, HistoryFRResponse } from 'api/history';
 import { leadingZeros } from 'utils/dateFormat';
+import LoadingSpinner from 'components/Loading/Spinner';
 
 export interface IForm {
   pageSize: number | null;
@@ -26,8 +26,9 @@ export interface Action {
     | 'page'
     | 'searchDateFrom'
     | 'searchDateTo'
-    | 'resultCd';
-  payload: string | number | null;
+    | 'resultCd'
+    | 'init';
+  payload?: string | number | null;
 }
 
 const getDefaultDateFrom = () => {
@@ -52,6 +53,7 @@ const initialState: IForm = {
   resultCd: -1,
 };
 function formReducer(state: IForm, action: Action) {
+  if (action.type === 'init') return initialState;
   return { ...state, [action.type]: action.payload };
 }
 interface Props {
@@ -86,9 +88,6 @@ function Search({ curPage, setCurPage }: Props) {
   }, [data]);
   const onSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    dispatch(
-      historyFRSlice.actions.updateHistoryFRState({} as HistoryFRResponse),
-    );
     refetch();
   };
 
@@ -96,82 +95,90 @@ function Search({ curPage, setCurPage }: Props) {
     console.log(formState);
   }, [formState]);
   return (
-    <div className="px-2 rounded shadow bg-white">
-      <form onSubmit={onSubmit} className="flex py-2 px-4 items-center">
-        <span className="mr-3">인증요청일자</span>
-        <div className="relative">
-          <input
-            type="date"
-            className="block px-5 py-2 outline-0  text-sm text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-            required
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              formDispatch({
-                type: 'searchDateFrom',
-                payload: event.currentTarget.value,
-              })
-            }
-            value={formState.searchDateFrom}
-          />
-        </div>
-        <span className="mx-3 text-xl">~</span>
-        <div className="relative">
-          <input
-            type="date"
-            className="block px-5 py-2 outline-0  text-sm text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-            required
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              formDispatch({
-                type: 'searchDateTo',
-                payload: event.currentTarget.value,
-              })
-            }
-            value={formState.searchDateTo}
-          />
-        </div>
-        <span className="ml-5 mr-3">인증결과</span>
+    <div className="flex items-center">
+      <div className="px-2 rounded shadow bg-white">
+        <form onSubmit={onSubmit} className="flex py-2 px-4 items-center">
+          <span className="mr-3">인증요청일자</span>
+          <div className="relative">
+            <input
+              type="date"
+              className="block px-5 py-2 outline-0  text-sm text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+              required
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                formDispatch({
+                  type: 'searchDateFrom',
+                  payload: event.currentTarget.value,
+                })
+              }
+              value={formState.searchDateFrom}
+            />
+          </div>
+          <span className="mx-3 text-xl">~</span>
+          <div className="relative">
+            <input
+              type="date"
+              className="block px-5 py-2 outline-0  text-sm text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+              required
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                formDispatch({
+                  type: 'searchDateTo',
+                  payload: event.currentTarget.value,
+                })
+              }
+              value={formState.searchDateTo}
+            />
+          </div>
+          <span className="ml-5 mr-3">인증결과</span>
 
-        <select
-          onChange={(event: ChangeEvent<HTMLSelectElement>) =>
-            formDispatch({
-              type: 'resultCd',
-              payload: event.currentTarget.value,
-            })
-          }
-          value={formState.resultCd ?? undefined}
-          className="px-2 py-2 outline-0 text-sm  text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value={-1} defaultChecked>
-            전체
-          </option>
-          <option value={1}>성공</option>
-          <option value={0}>실패</option>
-        </select>
-        <button
-          type="submit"
-          className="text-white  bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 mx-2 "
-        >
-          <svg
-            className="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+          <select
+            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+              formDispatch({
+                type: 'resultCd',
+                payload: event.currentTarget.value,
+              })
+            }
+            value={formState.resultCd ?? undefined}
+            className="px-2 py-2 outline-0 text-sm  text-gray-900 bg-[#F9F9F9] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </button>
-        <button
-          type="submit"
-          className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
-        >
-          초기화
-        </button>
-      </form>
+            <option value={-1} defaultChecked>
+              전체
+            </option>
+            <option value={1}>성공</option>
+            <option value={0}>실패</option>
+          </select>
+          <button
+            type="submit"
+            className="text-white  bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 mx-2 "
+          >
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              formDispatch({
+                type: 'init',
+              })
+            }
+            className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
+          >
+            초기화
+          </button>
+        </form>
+      </div>
+      {isFetching && <LoadingSpinner />}
     </div>
   );
 }
